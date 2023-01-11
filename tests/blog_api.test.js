34cf38be02c2de1900dfe819f9ba2771b9fac3ca
mstitane <mohamed.stitane@opencellsoft.com>
2023-01-11 19:02:15 +0100

    finished exercise 4.14 Blog list expansions, step2

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/Blog')
-const { ObjectId } = require('mongodb')
 const api = supertest(app)
 
 const initialBlogs = [
@@ -32,96 +31,115 @@ beforeEach(async () => {
     await noteObject.save()
 })
 
-test('blogs are returned as json', async () => {
-    const response = await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-    expect(response.body).toHaveLength(2)
-})
+describe('when there is initially some blogs saved', () => {
 
-test('the unique identifier property of the blog posts is named id', async () => {
-    const response = await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-    expect(response.body[0].id).toBeDefined()
-})
+    test('blogs are returned as json', async () => {
+        const response = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        expect(response.body).toHaveLength(2)
+    })
 
-test('successfully creates a new blog post', async () => {
-    const newBlog = {
-        title: 'Canonical string reduction',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-        likes: 12,
-    }
+    test('the unique identifier property of the blog posts is named id', async () => {
+        const response = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        expect(response.body[0].id).toBeDefined()
+    })
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+    test('successfully creates a new blog post', async () => {
+        const newBlog = {
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+            likes: 12,
+        }
 
-    const response = await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-    expect(response.body).toHaveLength(initialBlogs.length + 1)
-
-    const contents = response.body.map(n => n.title)
-    expect(contents).toContain(
-        'Canonical string reduction'
-    )
-})
-
-test('if the likes property is missing from the request, it will default to 0', async () => {
-    const newBlog = {
-        title: 'First class tests',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    }
-
-    const response = await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-    expect(response.body.likes).toBe(0)
-
-})
-
-test('if the title or url properties are missing, it will return 400', async () => {
-    const newBlogWithoutTitle = {
-        author: 'TEST MISSING TITLE',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlogWithoutTitle)
-        .expect(400)
-
-    const newBlogWithoutUrl = {
-        author: 'TEST MISSING URL',
-        title: 'Test title',
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlogWithoutUrl)
-        .expect(400)
-
-})
-
-describe('deleting of blog', function () {
-    const blogToDelete = new Blog(initialBlogs[0])
-    test('succeeds with status code 204 if id is valid', async () => {
         await api
-            .delete(`/api/blogs/${blogToDelete._id}`)
-            .expect(204)
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        expect(response.body).toHaveLength(initialBlogs.length + 1)
+
+        const contents = response.body.map(n => n.title)
+        expect(contents).toContain(
+            'Canonical string reduction'
+        )
+    })
+
+    test('if the likes property is missing from the request, it will default to 0', async () => {
+        const newBlog = {
+            title: 'First class tests',
+            author: 'Robert C. Martin',
+            url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+        }
+
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        expect(response.body.likes).toBe(0)
+
+    })
+
+    test('if the title or url properties are missing, it will return 400', async () => {
+        const newBlogWithoutTitle = {
+            author: 'TEST MISSING TITLE',
+            url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlogWithoutTitle)
+            .expect(400)
+
+        const newBlogWithoutUrl = {
+            author: 'TEST MISSING URL',
+            title: 'Test title',
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlogWithoutUrl)
+            .expect(400)
+
+    })
+
+    describe('deleting of blog', function () {
+        const blogToDelete = new Blog(initialBlogs[0])
+        test('succeeds with status code 204 if id is valid', async () => {
+            await api
+                .delete(`/api/blogs/${blogToDelete._id}`)
+                .expect(204)
+            const response = await api
+                .get('/api/blogs')
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
+            expect(response.body).toHaveLength(initialBlogs.length - 1)
+        })
+    })
+    describe('updating a blog', function () {
+        test('succeeds with status code 204 if id is valid', async () => {
+            const initialBlog = new Blog(initialBlogs[0])
+
+            const likes = 20
+            const updated = await api.put(`/api/blogs/${initialBlog._id}`)
+                .send({ likes: likes })
+                .expect(200)
+
+            expect(updated.body.likes).toBe(likes)
+        })
     })
 })
-
 afterAll(() => {
     mongoose.connection.close()
 })
